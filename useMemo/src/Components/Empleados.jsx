ed685a4d9e09322707f72c60dca59bf6d3370a2b
MichellMurallas <michellsms893@gmail.com>
2023-03-07 19:53:12 -0500

    ejemp1-useMemo

--- useMemo/src/Components/Empleados.jsx
+++ useMemo/src/Components/Empleados.jsx
@@ -1,36 +1,43 @@
 import React, { useEffect, useState } from 'react'
 
-export const Empleados = ({pagina =1} ) => {
+export const Empleados = React.memo(
+  ({pagina} ) => {
 
-  const [empleados, setEmpleados] = useState([])
+    const [empleados, setEmpleados] = useState([])
+  
+    useEffect(() => {
+      conseguirEmpleados(pagina)
+    }, [pagina])
+  
+    const conseguirEmpleados = async(p)=>{
+      const url = "https://reqres.in/api/users?page="+p;
+      const peticion = await fetch(url);
+      const {data: empleados} = await peticion.json();
+  
+      setEmpleados(empleados)
+      console.log(empleados)
+    }
 
-  useEffect(() => {
-    conseguirEmpleados(pagina)
-  }, [])
-
-  const conseguirEmpleados = async(p)=>{
-    const url = "https://reqres.in/api/users?page" + p;
-    const peticion = await fetch(url);
-    const {data: empleados} = await peticion.json();
-
-    setEmpleados(empleados)
-    console.log(empleados)
+    useEffect(() => {
+    
+    }, [empleados])
+  
+  
+    return (
+      <div>
+        <p>Mostrnado la pagina: {pagina}</p>
+        <ul>
+          {
+            empleados.map((empleado)=>{
+              return(
+                <li key={empleado.id}>
+                  {empleado.first_name + " " + empleado.email }
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
   }
-
-
-  return (
-    <div>
-      <ul>
-        {
-          empleados.map((empleado)=>{
-            return(
-              <li key={empleado.id}>
-                {empleado.first_name + " " + empleado.email }
-              </li>
-            )
-          })
-        }
-      </ul>
-    </div>
-  )
-}
+)
